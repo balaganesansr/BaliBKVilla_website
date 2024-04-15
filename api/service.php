d3cf0730306d422f80b6503733331871579f85da
balaganesansr <balaganesansr@gmail.com>
2024-04-15 23:20:42 +0530

    #

--- api/service.php
+++ api/service.php
@@ -1,7 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: http://localhost:5173");
-header("Access-Control-Allow-Methods: POST");
-header("Access-Control-Allow-Headers: Content-Type");
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
    
     $name = trim($_POST["name"]);
